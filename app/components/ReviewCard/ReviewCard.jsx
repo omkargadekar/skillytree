8cd98538819fb28344eb8c3de9170f43e6ad6b9a
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-04-06 09:42:13 +0530

    add final

--- app/components/ReviewCard/ReviewCard.jsx
+++ app/components/ReviewCard/ReviewCard.jsx
@@ -1,29 +1,33 @@
+// ReviewCard.jsx
 import styles from "./ReviewCard.module.css";
+import Image from "next/image";
 
 const ReviewCard = () => {
   return (
     <div className={styles.reviewCard}>
-      <div className={styles.decorationCircle}></div>
-      <div className={styles.decorationCircle}></div>
-      <div className={styles.customerInfo}>
-        <img
-          src="/profile-picture.jpg"
-          alt="Profile Picture"
-          className={styles.profilePicture}
+      <div className={styles.reviewerInfo}>
+        <Image
+          width={112}
+          height={112}
+          src="/man.png"
+          alt="Reviewer"
+          className={styles.reviewerAvatar}
         />
-        <div>
-          <h3 className={styles.customerName}>Andrew Holt</h3>
-          <p className={styles.customerEmail}>Graphics123@Gmail.Com</p>
+        <div className={styles.reviewerDetails}>
+          <h3 className={styles.reviewerName}>Andrew Holt</h3>
+          <p className={styles.reviewerEmail}>Graphics123@gmail.com</p>
         </div>
       </div>
-      <p className={styles.reviewText}>
-        I Had Been Dreaming To Learn Guitar For 4 Yrs. One Day I Visited
-        Peppertree.Com. I Clicked "Buy A Trial Session". Later All Happened
-        Automatically. I Kept Attending The Amazing Tutor Online And She Kept Me
-        Motivated. It's Been 4 Weeks & 40 Sessions. And Woohoo! I Just Surprised
-        All My Friends At A Party With My Skills! Stop Desiring A Skill, Just
-        Start With Peppertree!
-      </p>
+      <div className={styles.reviewContent}>
+        <p className={styles.reviewText}>
+          I Had Been Dreaming To Learn Guitar For 4 Yrs. One Day I Visited
+          Peppertree.Com. I Clicked "Buy A Trial Session". Later All Happened
+          Automatically, I Kept Attending The Amazing Tutor Online And She Kept
+          Me Motivated. It's Been 4 Weeks & 40 Sessions, And Woohoo! I Just
+          Surprised All My Friends At A Party With My Skills! Stop Desiring A
+          Skill, Just Start With Peppertree!
+        </p>
+      </div>
     </div>
   );
 };
--- app/components/Reviews/Reviews.jsx
+++ app/components/Reviews/Reviews.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ReviewCard from "../ReviewCard/ReviewCard";
+import styles from "./Reviews.module.css";
 
 const Reviews = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleDotClick = (index) => {
+    setActiveIndex(index);
+  };
   return (
-    <div style={{ backgroundColor: "white", height: "100vh" }}>
-      <h2>REVIEWS FROM OUR CUSTOMERS</h2>
+    <div style={{ height: "100vh", marginTop: "10rem", position: "relative" }}>
+      <h2 className={styles.head}>
+        REVIEWS FROM OUR <span className={styles.headspan}>CUSTOMERS</span>
+      </h2>
       <ReviewCard />
+      <p className={styles.circle}></p>
+      <p className={styles.circle1}></p>
+      <p className={styles.circle2}></p>
+      <p className={styles.circle3}></p>
+      <img className={styles.left} src="/right.png" alt="string left" />
+      <img className={styles.right} src="/left.png" alt="string right" />
+      <div className={styles.reviewdots}>
+        {[0, 1, 2, 4].map((_, index) => (
+          <span
+            key={index}
+            className={`${styles.dots} ${
+              activeIndex === index ? styles.active : ""
+            }`}
+            onClick={() => handleDotClick(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
